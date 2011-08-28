068fd4d78eb700a219949d968caf4ba69b4427e4
Ole Lynge Sørensen <ole@uncas.dk>
2011-08-28 11:28:55 +0200

    Adding move instances...

--- ChessTests.py
+++ ChessTests.py
@@ -2,20 +2,31 @@ import unittest
 
 
 class ChessTests(unittest.TestCase):
+
     def setUp(self):
-        print("*")
+        self.game = ChessGame()
 
     def test_InitialGame_PossibleMoves_20(self):
-        game = ChessGame()
-        self.assertEqual(20, len(game.PossibleMoves()))
+        self.game = ChessGame()
+        moves = self.game.PossibleMoves()
+        self.assertEqual(20, len(moves))
+        firstMove = moves[0]
+        fromSquare = firstMove.From
 
 
 class ChessGame:
-    def __init__(self):
-        print("Chess game starting...")
 
     def PossibleMoves(self):
-        return range(20)
+        result = []
+        for move in range(20):
+            result.append(Move())
+        return result
+
+
+class Move:
+
+    def __init__(self):
+        self.From = 0
 
 
 if __name__ == "__main__":
