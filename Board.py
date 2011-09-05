e364fff62b9f40e57f6c1044f960e6ee942a0781
Ole Lynge Sørensen <ole@uncas.dk>
2011-09-05 21:55:56 +0200

    Refactored.

--- Board.py
+++ Board.py
@@ -16,29 +16,29 @@ class Board:
 
     def __init__(self):
         self.Pieces = []
-        self.addPieces()
+        self._addPieces()
 
     def GetPiece(self, position):
         for piece in self.Pieces:
             if piece.Position == position:
                 return piece
 
-    def addPieces(self):
+    def _addPieces(self):
         for file in File.All:
-            self.addPiece(Color.White, Kind.Pawn, file, Board.WhitePawnRank)
-            self.addPiece(Color.Black, Kind.Pawn, file, Board.BlackPawnRank)
-        self.addOfficers(Color.White, Board.WhiteOfficerRank)
-        self.addOfficers(Color.Black, Board.BlackOfficerRank)
+            self._addPiece(Color.White, Kind.Pawn, file, Board.WhitePawnRank)
+            self._addPiece(Color.Black, Kind.Pawn, file, Board.BlackPawnRank)
+        self._addOfficers(Color.White, Board.WhiteOfficerRank)
+        self._addOfficers(Color.Black, Board.BlackOfficerRank)
 
-    def addOfficers(self, color, rank):
+    def _addOfficers(self, color, rank):
         for file in Board.RookFiles:
-            self.addPiece(color, Kind.Rook, file, rank)
+            self._addPiece(color, Kind.Rook, file, rank)
         for file in Board.KnightFiles:
-            self.addPiece(color, Kind.Knight, file, rank)
+            self._addPiece(color, Kind.Knight, file, rank)
         for file in Board.BishopFiles:
-            self.addPiece(color, Kind.Bishop, file, rank)
-        self.addPiece(color, Kind.Queen, Board.QueenFile, rank)
-        self.addPiece(color, Kind.King, Board.KingFile, rank)
+            self._addPiece(color, Kind.Bishop, file, rank)
+        self._addPiece(color, Kind.Queen, Board.QueenFile, rank)
+        self._addPiece(color, Kind.King, Board.KingFile, rank)
 
-    def addPiece(self, color, kind, file, rank):
+    def _addPiece(self, color, kind, file, rank):
         self.Pieces.append(Piece(color, kind, Square(file, rank)))
