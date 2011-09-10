1083d2dc4b412a057125971ba0e381059dae85cb
Ole Lynge Sørensen <ole@uncas.dk>
2011-09-10 23:17:17 +0200

    Refactored.

--- Move.py
+++ Move.py
@@ -1,12 +1,13 @@
 class Move:
     
-    def __init__(self, origin, destination):
-        self.Origin = origin
+    def __init__(self, piece, destination):
+        self.Piece = piece
         self.Destination = destination
+        self.Origin = piece.Position
         self.IsCastling = False
 
     def __repr__(self):
-        return "Move from " + str(self.Origin) + " to " + str(self.Destination) + "."
+        return "Move " + str(self.Piece) + " from " + str(self.Origin) + " to " + str(self.Destination) + "."
 
     def __eq__(self, other):
         return self.Origin == other.Origin and self.Destination == other.Destination
@@ -19,4 +20,4 @@ class Move:
 
     @staticmethod
     def Normal(piece, destination):
-        return Move(piece.Position, destination)
+        return Move(piece, destination)
