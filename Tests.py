80b6f4a528b4b8728f2523eb0409c502f09cbceb
Ole Lynge Sørensen <ole@uncas.dk>
2011-10-06 23:22:07 +0200

    Refactoring tests.

--- Tests.py
+++ Tests.py
@@ -1,9 +1,9 @@
 import time
 import unittest
 
-if __name__ == "__main__":
+def Test(pattern):
     time1 = time.time()
-    suite = unittest.TestLoader().discover(".")
+    suite = unittest.TestLoader().discover(".", pattern)
     result = unittest.TestResult()
     suite.run(result)
     print(result)
@@ -13,3 +13,12 @@ if __name__ == "__main__":
         print(error)
     time2 = time.time()
     print("Test duration: " + str(time2-time1) + " seconds")
+
+def TestFast():
+    Test("test_*.py")
+
+def TestSlow():
+    Test("testslow_*.py")
+
+if __name__ == "__main__":
+    TestFast()
--- test_Chess.py
+++ test_Chess.py
@@ -7,7 +7,6 @@ from Move import *
 from Piece import *
 from PieceMoveGenerator import *
 
-
 class MoveTests(unittest.TestCase):
 
     def setUp(self):
@@ -584,48 +583,6 @@ class ChessTests(unittest.TestCase):
 
     def _possibleMoves(self):
         return self.game.PossibleMoves()
-
-
-class ChessEngineTests(unittest.TestCase):
-
-    def setUp(self):
-        self._game = ChessGame()
-        self._engine = ChessEngine(self._game)
-
-    def xtest_3BestMoves_Returns3Moves(self):
-        bestMoves = self._engine.BestMoves(3)
-        self.assertEqual(3, len(bestMoves))
-
-    def xtest_10PlySequence_WithoutErrors(self):
-        for moveIndex in range(10):
-            bestMoves = self._engine.BestMoves(1)
-            move = bestMoves[0].Move
-            self._game.ApplyMove(move)
-
-    def xtest_WhiteCanCapturePawn_CaptureIsBestMove(self):
-        self._game.Move(Square(File.D, 2), Square(File.D, 4))
-        self._game.Move(Square(File.E, 7), Square(File.E, 5))
-
-        bestMoves = self._engine.BestMoves(1)
-
-        piece = self._game.GetPiece(File.D, 4)
-        expected = Move(piece, Square(File.E, 5))
-        self.assertEqual(1, len(bestMoves))
-        self.assertEqual(expected, bestMoves[0].Move)
-
-    def xtest_WhiteCanGiveAwayPawn_OtherMovesAreBetter(self):
-        self._game.Move(Square(File.A, 2), Square(File.A, 4))
-        self._game.Move(Square(File.A, 7), Square(File.A, 5))
-        self._game.Move(Square(File.B, 2), Square(File.B, 3))
-        self._game.Move(Square(File.B, 7), Square(File.B, 6))
-
-        bestMoves = self._engine.BestMoves(1)
-
-        piece = self._game.GetPiece(File.B, 3)
-        #        print(bestMoves[0].Move)
-        notExpected = Move(piece, Square(File.B, 4))
-        self.assertEqual(1, len(bestMoves))
-        self.assertNotEqual(notExpected, bestMoves[0].Move)
 
 
 class EvaluationServiceTests(unittest.TestCase):
